d4a2d61a9f20f4ea8f52524e1a37ffbaa27bef4f
OmarCypha700 <98944012+OmarCypha700@users.noreply.github.com>
2024-06-26 15:40:32 -0700

    clean directory

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ with open(input_file, 'rb') as f_in:
 
 app = Flask('churn')
 
-# @app.route('/', methods=['GET']) # Homepage
-# def home():
-
-#     return render_template('index.html')
-
-
 @app.route('/', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
@@ -32,8 +26,6 @@ def predict():
     # For GET request, render the template without results    
     return render_template('index.html')
    
-
-
 if __name__ == '__main__':
     # app.run(debug=True, host='0.0.0.0', port=9696)
     app.run(debug=True)
